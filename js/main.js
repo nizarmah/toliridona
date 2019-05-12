720e247879435b6155934d3894e1e4659889c298
Nizar Mahmoud <nizarmah@hotmail.com>
2019-05-12 19:15:28 +0300

    Fixed Issue with Dynamic Poem Loading

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ function readPoem(poem, callback) {
                 var allText	= rawFile.responseText;
                 allText		= allText.split("#");
                 allText[3]	= allText[3].split(">");
+                allText[3]	= allText[3].slice(1, allText[3].length);
 
                 return callback(allText);
             }
@@ -21,7 +22,7 @@ function readPoem(poem, callback) {
 function windowLoad() {
 	num_poems = 0;
 	poems.forEach(function (poem) {
-		readPoem(poem, function (content) {
+		readPoem(poem, function (data) {
 			var li_poem			= document.createElement("LI");
 			if (num_poems == 0)
 				li_poem.className = "uk-open";
@@ -31,13 +32,13 @@ function windowLoad() {
 			title.className		= "uk-accordion-title";
 			title.href			= "#";
 
-			var text_title		= document.createTextNode(content[1].trim());
+			var text_title		= document.createTextNode(data[1].trim());
 			title.appendChild(text_title);
 			li_poem.appendChild(title);
 
 			var content			= document.createElement("UL");
 			content.className	= "uk-accordion-content";
-			content[3].forEach(function (line) {
+			data[3].forEach(function (line) {
 				line = line.trim();
 				if (line.length == 0)
 					line = "&nbsp;";
