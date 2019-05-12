ddb46531f41361bdf64bd62a6ee5244f181871d8
Nizar Mahmoud <nizarmah@hotmail.com>
2019-05-12 22:24:25 +0300

    Added MoJS Hearts Animation

---
+++ js/hearts.js
@@ -0,0 +1,48 @@
+class Heart extends mojs.CustomShape {
+	getShape () {
+		return '<path xmlns="http://www.w3.org/2000/svg" d="M 65,29 C 59,19 49,12 37,12 20,12 7,25 7,42 7,75 25,80 65,118 105,80 123,75 123,42 123,25 110,12 93,12 81,12 71,19 65,29 z"/>';
+	}
+}
+
+mojs.addShape( 'heart', Heart );
+
+const main_heart = new mojs.Shape({
+	left    : -5, 
+	top     : -25,
+	shape   : 'heart',
+	fill    : '#E5214A',
+	scale   : { 0 : 20 },
+	angle   : -10,
+	easing  : 'elastic.out',
+	duration: 1600,
+	delay   : 0,
+	radius  : 1
+});
+
+const mini_heart = new mojs.Shape({
+	left    : 25, 
+	top     : -55,
+	shape   : 'heart',
+	fill    : '#E5214A',
+	scale   : { 0 : 10 },
+	angle   : 10,
+	easing  : 'elastic.out',
+	duration: 1600,
+	delay   : 200,
+	radius  : 1
+});
+
+document.body.addEventListener('click', function (e) {
+	const coords = { 
+		x: e.pageX, 
+		y: e.pageY
+	};
+	
+	main_heart
+		.tune( coords )
+		.replay();
+	
+	mini_heart
+		.tune( coords )
+		.replay();
+});
